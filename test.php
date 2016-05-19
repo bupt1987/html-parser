081b77141be0eb4847b44e9af0ac136783d080eb
slince <taosikai@yeah.net>
2016-05-19 21:45:33 +0800

    修改代码为psr风格

--- test.php
+++ test.php
@@ -4,22 +4,23 @@ require 'vendor/autoload.php';
 $url = 'http://www.sina.com.cn/';
 $sHtml = file_get_contents($url);
 
-for($i = 0; $i < 10000; $i ++) {
-	test($sHtml);
-	if($i % 100 == 0) {
-		echo $i . ' ';
-		echo round(memory_get_usage() / 1024 / 1024, 3) . 'M, ';
-		echo round(memory_get_peak_usage() / 1024 / 1024, 3) . 'M' . "\n";
-	}
+for ($i = 0; $i < 10000; $i++) {
+    test($sHtml);
+    if ($i % 100 == 0) {
+        echo $i . ' ';
+        echo round(memory_get_usage() / 1024 / 1024, 3) . 'M, ';
+        echo round(memory_get_peak_usage() / 1024 / 1024, 3) . 'M' . "\n";
+    }
 }
 
 echo round(memory_get_usage() / 1024 / 1024, 3) . 'M, ';
 echo round(memory_get_peak_usage() / 1024 / 1024, 3) . 'M' . "\n";
 
-function test($sHtml) {
-	$oDom = new \HtmlParser\ParserDom($sHtml);
-	$oDom->find('ul.uni-blk-list02', 0);
-	$oDom->find('a');
-	$oDom->find('ul');
-	$oDom->find('p');
+function test($sHtml)
+{
+    $oDom = new \HtmlParser\ParserDom($sHtml);
+    $oDom->find('ul.uni-blk-list02', 0);
+    $oDom->find('a');
+    $oDom->find('ul');
+    $oDom->find('p');
 }
